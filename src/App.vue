38b25c5b08f8ef7cc7cb7888eef8ea240e0a6eaf
hvekriya <h.vekriya@kainos.com>
2018-10-18 12:01:35 +0100

    Skeleton app

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <Nav></Nav>
+  </div>
+</template>
+
+<script>
+import Nav from '@/components/nav'
+export default {
+  name: 'app',
+  components: {
+    Nav: Nav
+  },
+  head: {
+    meta: [
+      { name: 'viewport', content: 'width=device-width, initial-scale=1'}
+    ]
+  }
+}
+</script>
---
+++ src/components/nav.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <b-navbar type="dark" variant="info" toggleable="lg">
+
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+    
+      <b-navbar-brand href="/" class="d-lg-none">Harry's Gym</b-navbar-brand>
+    
+      <b-collapse is-nav id="nav_collapse">
+        <b-navbar-nav>
+          <b-nav-item href="#">Home</b-nav-item>
+          <b-nav-item href="#">Protein</b-nav-item> 
+          <b-nav-item href="#">Apparel</b-nav-item>       
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Nav'
+  }
+</script>
+
+<style>
+
+</style>
